c02569114fb89abbcc333564a0c9fb2222e2fe8a
gungho0619 <tzztntt@gmail.com>
2022-07-12 15:20:46 -0700

    feat: add pagination parameter to the service

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -7,17 +7,14 @@ import { SearchResult } from '../models/http.model';
 import { User } from '../models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  search(login: string): Observable<SearchResult<User>> {
+  search(login: string, page = 1, perPage = 9): Observable<SearchResult<User>> {
     const url = `${environment.api}/search/users`;
-    const params = new HttpParams().append('q', `${login} in:login`);
+    const params = new HttpParams().append('q', `${login} in:login`).append('per_page', perPage).append('page', page);
     return this.http.get<SearchResult<User>>(url, { params });
   }
 }
