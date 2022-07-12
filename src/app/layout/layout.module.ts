925bff2fa8389ef257af616942032b103e19d41b
gungho0619 <tzztntt@gmail.com>
2022-07-12 15:36:45 -0700

    feat: navbar implementation

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { LayoutComponent } from './layout.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [LayoutComponent, NavbarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatIconModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
