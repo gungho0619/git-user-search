d1e2a2f373e9aab95704367bcddf662c5e6eab24
gungho0619 <tzztntt@gmail.com>
2022-07-12 18:27:23 -0700

    chore: write unit test for the search bar component

--- src/app/pages/search-users/search-bar/search-bar.component.spec.ts
+++ src/app/pages/search-users/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SearchBarComponent } from './search-bar.component';
+import { SpinnerComponent } from '../../../shared/spinner/spinner.component';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -8,9 +16,17 @@ describe('SearchBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
-    })
-    .compileComponents();
+      imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+      ],
+      declarations: [SearchBarComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
@@ -19,5 +35,28 @@ describe('SearchBarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render button and label of input box', () => {
+    const fixture = TestBed.createComponent(SearchBarComponent);
+    const searchBarComponent = fixture.componentInstance;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('button span.font-medium')?.textContent).toContain('Submit');
+    expect(compiled.querySelector('mat-label')?.textContent).toContain('Search by Login');
+  });
+
+  it('should send keyword when do submit', (done: DoneFn) => {
+    const fixture = TestBed.createComponent(SearchBarComponent);
+    const searchBarComponent = fixture.componentInstance;
+    fixture.detectChanges();
+    searchBarComponent.form.controls.keyword.setValue('webcat');
+
+    searchBarComponent.search.asObservable().subscribe((res) => {
+      expect(res).toContain('webcat');
+      done();
+    });
+
+    searchBarComponent.submit();
   });
 });
